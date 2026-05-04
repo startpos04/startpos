f6bc9aebe2419bf7940e98a139b136c3f385d95f
rj <rrlopez06@yahoo.com>
2026-05-04 19:51:49 +0800

    fix hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,11 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
-import path from 'path'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackSerwistPlugin } from './vite-plugin'
 
 const config = defineConfig({
-  resolve: {
-    alias: {
-      // This tells Vite: when you see "prisma/", look in the local folder, not node_modules
-      'prisma/': `${path.resolve(__dirname, 'prisma')}/`,
-    },
-  },
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
